fb5c9d1a8c4bb24199a4a9649ce69927395ab88f
Mark Wolfe <mark.wolfe@buildkite.com>
2024-09-30 15:27:21 +1000

    Update build script to setup Config.xcconfig

--- .buildkite/scripts/build.sh
+++ .buildkite/scripts/build.sh
@@ -6,4 +6,9 @@ SOURCE=${BASH_SOURCE[0]}
 
 BUILDKITE_CACHE_MOUNT_PATH="${NSC_CACHE_PATH:-.cache/buildkite}"
 
+sed \
+    -e "s|__SENTRY_DSN_HOST_REPLACE__|$(buildkite-agent secret get sentry_dsn_host)|g" \
+    -e "s|__SENTRY_DSN_PATH__|$(buildkite-agent secret get sentry_dsn_path)|g" \
+    Config.xcconfig.template > Config.xcconfig
+
 xcodebuild clean build -clonedSourcePackagesDirPath SwiftPackages CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
