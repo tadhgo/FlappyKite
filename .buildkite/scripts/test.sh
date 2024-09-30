58b1254603faef658792427342978777085d3d31
Mark Wolfe <mark.wolfe@buildkite.com>
2024-09-30 16:01:32 +1000

    Add missing configuration for test run

--- .buildkite/scripts/test.sh
+++ .buildkite/scripts/test.sh
@@ -9,5 +9,11 @@ BUILDKITE_CACHE_MOUNT_PATH="${NSC_CACHE_PATH:-.cache/buildkite}"
 echo "--- :homebrew: Installing dependencies :homebrew:"
 brew install xcbeautify
 
+echo "--- :bash: Configure application for testing :bash:"
+sed \
+    -e "s|__SENTRY_DSN_HOST_REPLACE__||g" \
+    -e "s|__SENTRY_DSN_PATH__|$(|g" \
+    Config.xcconfig.template > Config.xcconfig
+
 echo "--- :xcode: Testing :xcode:"
 xcodebuild clean test -clonedSourcePackagesDirPath SwiftPackages -scheme FlappyKite -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 16,OS=18.0' | xcbeautify
